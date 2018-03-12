cd1c8c10eb364718559c20eb30e689b76ac2931e
taner94 <yldrmtnr@windowslive.com>
2018-03-12 16:14:40 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,45 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#inculude <fcntl.h>
-#inculude <sys/types.h>
-#inculude <errno.h>
-#difine BUF_SIZE 8192
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+#define BUF_SIZE 8192
 
-int main(int argc, char *argv[]) {
+int main(int argc) {
 	char kaynak[10];
 	char hedef [10];
 	int filedes_input, filedes_output;
-	ssize_t returned_input, returned_output;
-	char buf[BUF_SÝZE];
+g	ssize_t returned_input, returned_output;
+	char buf[BUF_SIZE];
+	//kaynak dosya
 	printf("kaynak dosya adini giriniz : ");
 	scanf("%s",&kaynak);
 	filedes_input=open(kaynak, O_RDONLY);
-			if(filedes_input==-1)
-				{
-					perror("KAYNAK DOSYA BULUNAMADI!!\N LUTFEN GECERLÝ DOSYA ADINI GIRINIZ...\N");
-					return 2;
-				}
-printf("hedef dosya adini gitiniz:");
-scanf("%s", &hedef);
-filedes_output=open(hedef, O_WRONLY);
-			if(filedes_output==-1)
-				{
-					perror("KAYNAK DOSYA BULUNAMADI!!\N LUTFEN GECERLÝ DOSYA ADINI GIRINIZ...\N");
-					return 3;
-				}
-while((returned_output=write(filedes_input, &buf, BUF_SIZE))>0)
-				{
-				returned_output=write(filedes_output, &buf,(ssize_t)returned_input);
-					if(returned_output!=returned_input)
-						{
-							perror("DOSYA YAZILMADI");
-							return 4;
-						}	
-				}	
-close(filedes_input);
-close(filedes_output);
-return (EXIT_SUCCESS);	
-	
-	return 0;
+	if(filedes_input==-1)
+	{gg
+		perror("KAYNAK DOSYA BULUNAMADI!!");
+		return 2;
+	}
+	//Hedef dosya
+	printf("hedef dosya adini gitiniz:");
+	scanf("%s", &hedef);
+	filedes_output=open(hedef, O_WRONLY);g
+	if(filedes_output==-1)
+	{
+		perror("KAYNAK DOSYA BULUNAMADI!!");
+		return 3;
+	}
+
+	while((returned_input=read(filedes_input, &buf, BUF_SIZE))>0)
+	{
+	returned_output=write(filedes_output, &buf,(ssize_t)returned_input);
+		if(returned_output!=returned_input)
+		{
+			perror("DOSYA YAZILMADI");
+			return 4;
+		}	g
+	}	
+	close(filedes_input);
+	close(filedes_output);
+	return (EXIT_SUCCESS);	
 }
